f47d933680501d7d5360605683033ce2997d28c2
erwin <wienzzz@gmail.com>
2022-08-31 15:58:02 +0700

    test

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
-dotenv.config();
+dotenv.config();  
 
 export const mongoSampleMflix = mongoose.createConnection(process.env.MONGODB!).useDb("sample_mflix")
 
